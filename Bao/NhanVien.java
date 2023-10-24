494fa5db4f66577483ea4cd58df852cb4e593a66
Miryl <siinpy0102@gmail.com>
2023-10-24 10:40:36 +0700

    test

--- Bao/NhanVien.java
+++ Bao/NhanVien.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class c2b1 {
+public class NhanVien {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String name = sc.next();
